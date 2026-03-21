0c6e2a6dd2379ef6b51e8558400bf3ea8bcef443
arunkumar.tn <arunkumar.tn@ARUNKUMAR-WS>
2026-03-21 11:17:52 +0530

    Data Stuctures sample prctice programs

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string[] arr = { "flower", "flow", "flight" };
+            //string[] arr = { "dog", "racecar", "car" };
+            //string[] arr = {"","a"};
+            string[] arr = {"aaa", "aa","aaa"};
+            Console.WriteLine(LongestCommonPrefix(arr));
+            Console.ReadLine();
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string temp = strs[0];
+            string temp2 = "";
+            string tempstr = "";
+            string mainStr = "";
+            if (strs.Length>1)
+            {
+                for (int i = 1; i < strs.Length; i++)
+                {
+                    tempstr = strs[i];
+                    for (int j = 0; j < tempstr.Length; j++)
+                    {
+                        if (temp.Length > 0)
+                        {
+                            if (temp[j] == tempstr[j]  )
+                            {
+                                temp2 += tempstr[j];
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                       
+                    }
+                    mainStr = temp2;
+                    temp2 = "";
+                }
+            }
+            else
+            {
+                mainStr=strs[0];
+            }
+            
+            return mainStr;
+
+        }
+    }
+}
---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseString();
+            LargestNumber();
+            VowelsCount();
+            if (Palindrome())
+                Console.WriteLine("String is palindrome");
+            else
+                Console.WriteLine("String is not a palindrome");
+            Console.WriteLine($"Even numbers count {CountEvenNUmbers()}");
+
+            Console.WriteLine($"Sum numbers count {sumOfNumbers()}");
+            Console.ReadLine();
+        }
+
+        //Reverse of a string
+        public static void ReverseString()
+        {
+            string str = "Hello";
+            Console.WriteLine("Reverse of a string");
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                Console.Write(str[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+        //Largest of a number
+        public static void LargestNumber()
+        {
+            Console.WriteLine("Largest number in an array");
+            int[] arr = { 3, 7, 2, 9, 5 };
+            int large = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > large)
+                {
+                    large = arr[i];
+                }
+            }
+            Console.WriteLine($"Largest number in an array is { large}");
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        //Number of Vowels 
+        public static void VowelsCount()
+        {
+            Console.WriteLine($"Count Vowels");
+            string strr = "ProgrAmming";
+            strr = strr.ToLower();
+            int count = 0;
+            for (int i = 0; i < strr.Length; i++)
+            {
+                if (strr[i] == 'a' || strr[i] == 'e' || strr[i] == 'i' || strr[i] == 'o' || strr[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"vowels count = { count}");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        public static bool Palindrome()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Palindrome or not");
+            string str = "a";
+            int left = 0;
+            int right = str.Length - 1;
+            while (left < right)
+            {
+                if (str[left] != str[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+        public static int CountEvenNUmbers()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Even numbers count");
+            int[] numbers = { 1, 2, 3, 4, 6 };
+            int count = 0;
+            return  count = numbers.Count(x => x % 2 == 0);
+        }
+
+        public static int sumOfNumbers()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("sum of numbers");
+            int[] numbers = { 1, 2, 3, 4, 6 };
+            int count = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                count += numbers[i];
+            }
+            return count;
+        }
+    }
+}
