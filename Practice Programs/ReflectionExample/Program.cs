59ffb2d99316d4bb2e937e4c66b93e80505bfd16
arunkumar.tn <arunkumar.tn@ARUNKUMAR-WS>
2026-03-23 11:47:36 +0530

    Added reflection example projects

---
+++ Practice Programs/ReflectionExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using RefletctionClassLibrary;
+namespace ReflectionExample
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            var obj = new Class1();
+            Console.WriteLine(obj.sum(1,2));
+            
+            Console.WriteLine("All Public Fields");
+            Type type = obj.GetType();
+            foreach (MemberInfo memberInfo in type.GetFields())
+            {
+                Console.WriteLine(memberInfo.Name);
+            }
+
+            Console.WriteLine("\nAll Public Methods");
+            foreach (MemberInfo memberInfo in type.GetMethods())
+            {
+                Console.WriteLine(memberInfo.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practice Programs/RefletctionClassLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefletctionClassLibrary
+{
+    public class Class1
+    {
+        public int x;
+        public int y;
+        public int z;
+        public string Name { get; set; }
+        public int sum(int a, int b)
+        {
+            this.x = a;
+            this.y = b;
+            return z = x + y;
+        }
+        public int Mul(int a, int b) => a * b;
+       
+    }
+}
