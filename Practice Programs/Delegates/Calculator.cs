7cba7efad835fa462320b299a893dd6aae26b3e7
arunkumar.tn <arunkumar.tn@ARUNKUMAR-WS>
2026-03-26 18:46:08 +0530

    Implemented Delegates example

---
+++ Practice Programs/Delegates/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class Calculator
+    {
+        private int _result;
+        public int Add<T>(T a, T b)
+        {
+            if(a is int && b is int)
+            {
+                return _result = (int)(object)a + (int)(object)b;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+                return 0;
+            }
+        }
+        public int Mul<T>(T a, T b)
+        {
+            if (a is int && b is int)
+            {
+                return _result = (int)(object)a * (int)(object)b;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+                return 0;
+            }
+        }
+        public int Sub<T>(T a, T b)
+        {
+            if (a is int && b is int)
+            {
+                return _result = (int)(object)a - (int)(object)b;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+                return 0;
+            }
+        }
+        public int Div<T>(T a, T b)
+        {
+            if (a is int && b is int)
+            {
+                if ((int)(object)b ==0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    return 0;
+                }
+                return _result = (int)(object)a / (int)(object)b;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Practice Programs/Delegates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        //syntax: <modifier> delegate delegateName <return type> (parameter list);
+        public delegate int CalculatorDelegate (int a, int b);
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine("+,-,*,/");
+            char input = Convert.ToChar( Console.ReadLine());
+            switch (input)
+            {
+                case '+':
+                    CalculatorDelegate add = c.Add;
+                    Console.WriteLine(add(1, 2));
+                    break;
+                case '-':
+                    CalculatorDelegate sub = c.Sub;
+                    Console.WriteLine(sub(5,5));
+                    break;
+                case '*':
+                    CalculatorDelegate mul = c.Mul;
+                    Console.WriteLine(mul(8, 5));
+                    break;
+                case '/':
+                    CalculatorDelegate div = c.Div;
+                    Console.WriteLine(div(8, 5));
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+       
+       
+    }
+}
