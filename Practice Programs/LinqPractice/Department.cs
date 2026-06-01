17317a2782323b8d31cbab201ac02bd2e1708fa2
arunkumar.tn <arunkumar.tn@ARUNKUMAR-WS>
2026-06-01 19:14:03 +0530

    Linq select and selectMany added

---
+++ Practice Programs/LinqPractice/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public List<string> Employees { get; set; }
+    }
+}
---
+++ Practice Programs/LinqPractice/MainClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+          List<Department> d = new List<Department>
+          {
+              new Department
+              {
+                  Name = "HR",
+                  Employees = new List<string>{"Arun", "Kumar" }
+              },
+              new Department
+              {
+                  Name ="IT",
+                  Employees = new List<string> {"Jhon","Leo" }
+              }
+          };
+
+            var select = d.Select(x => x.Employees);
+            foreach(var item in select)
+            {
+                foreach(var item2 in item)
+                {
+                    Console.WriteLine(item2);
+                }
+            }
+            Console.WriteLine();
+            var selectMany = d.SelectMany(x => x.Employees);
+            foreach(var item in selectMany)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            foreach (var item in d)
+            {
+                Console.Write(item.Name + " -> ");
+                Console.WriteLine(string.Join(",", item.Employees));
+            }
+
+          Console.ReadLine();
+        }
+    }
+}
